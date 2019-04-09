06aae4fae9a8f1ca863a0da3d6bf5a9ececa6348
luoJiaJun <1768655042@qq.com>
2019-04-09 17:04:29 +0800

    init

---
+++ NetCore.Rabbitmq/NetCore.Rabbitmq.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore.Rabbitmq.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
